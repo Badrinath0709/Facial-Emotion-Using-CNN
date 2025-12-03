268f8b23c711e9adf19bc6f086044e07db90dd35
Badrinath0709 <badrisonic@gmail.com>
2025-12-03 10:29:01 +0530

    Update Convert CSV to HDF5.py

--- Face Emotion Recogniation Using CNN/newfolder/Convert CSV to HDF5.py
+++ Face Emotion Recogniation Using CNN/newfolder/Convert CSV to HDF5.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # ✅ Correct path (note the double backslash or raw string)
-csv_path = r"E:\t-hub\coding\project\Assessments\30-10-2025(Mini-Project)\emotion dataset\images\emotion_images.csv"
+csv_path = r"D:\Badri\New folder (2)\Facial-Emotion-Using-CNN\Face Emotion Recogniation Using CNN\emotion dataset\images\emotion_images.csv"
 
 # ✅ Check if file exists before reading
 if not os.path.exists(csv_path):
